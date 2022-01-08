3af1e04ace0ecf703c8b85620789d42bd02c809e
uncle-lv <uncle.lv@outlook.com>
2022-01-08 21:43:56 +0800

    ✨ feat(rand.py): random module
    random bool\int\char\str

---
+++ mock/rand.py
@@ -0,0 +1,67 @@
+import random
+import string
+from typing import List
+
+DEFAULT_MAX = 1 << 12
+DEFAULT_MIN = -DEFAULT_MAX
+DEFAULT_SIZE = 10
+
+
+class ValueException(Exception):
+    pass
+
+
+class TypeException(Exception):
+    pass
+
+
+def boolean() -> bool:
+    return bool(random.getrandbits(1))
+
+def booleans(size: int = DEFAULT_SIZE) -> List[bool]:
+    if size < 1:
+        raise ValueException('the size of a list must be at least 1')
+    
+    bools = []
+    for _ in range(0, size):
+        bools.append(boolean())
+    return bools
+    
+def natural(min: int = 0, max: int = DEFAULT_MAX) -> int:
+    if min < 0 or max < 0:
+        raise ValueException('the natural number must be equal or bigger than 0')
+    return random.randint(min, max)
+
+def naturals(min: int = 0, max: int = DEFAULT_MAX, size: int = DEFAULT_SIZE) -> List[int]:
+    if size < 1:
+        raise ValueException('the size of a list must be at least 1')
+    
+    naturals = []
+    for _ in range(0, size):
+        naturals.append(natural(min, max))
+    return naturals
+
+def integer(min: int = DEFAULT_MIN, max: int = DEFAULT_MAX) -> int:
+    return random.randint(min, max)
+
+def integers(min: int = DEFAULT_MIN, max: int = DEFAULT_MAX, size: int = DEFAULT_SIZE) -> List[int]:
+    if size < 1:
+        raise ValueException('the size of a list must be at least 1')
+    
+    integers = []
+    for _ in range(0, size):
+        integers.append(integer(min, max))
+    return integers
+
+def char(pool: str = string.ascii_letters) -> str:
+    if not isinstance(pool, str):
+        raise TypeException('pool value must be str, not \'{}\''.format(type(pool)))
+    
+    return random.choice(pool)
+
+def string(pool: str = string.ascii_letters, min: int = 3, max: int = 10) -> str:
+    if min < 2:
+        raise ValueException('The length of a string must ba at least 2')
+    
+    length = natural(min, max)
+    return ''.join([char(pool) for _ in range(length)])
